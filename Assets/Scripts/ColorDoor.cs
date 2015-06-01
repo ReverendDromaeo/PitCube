18f357dca553a3df8554439330a4d95696373d51
unknown <ChristianLangpaap@gmx.de>
2015-06-01 13:36:17 +0200

    Version vom 01.06.2015

---
+++ Assets/Scripts/ColorDoor.cs
@@ -0,0 +1,39 @@
+﻿////////////////////////////////////////////
+////                                    ////
+////   Autor: Jonas Fischer             ////
+////                                    ////
+////////////////////////////////////////////
+
+using UnityEngine;
+using System.Collections;
+
+public class ColorDoor : MonoBehaviour
+{
+    public AudioSource Open;
+    public AudioSource Correct;
+	
+    private bool doorIsOpen = false;
+
+    private void OpenDoor()
+    {
+        Correct.Play();
+        this.GetComponent<Animation>().Play();
+        doorIsOpen = true;
+        Open.Play(); 
+    }
+
+	void Update () 
+    {
+		//Überprüft ob alle Farben richtig sind und öffnet dann die Tür
+        if (GameObject.FindGameObjectWithTag("CCPink").GetComponent<ColorCube>().Match == true &&
+            GameObject.FindGameObjectWithTag("CCTurqouis").GetComponent<ColorCube>().Match == true &&
+            GameObject.FindGameObjectWithTag("CCOrange").GetComponent<ColorCube>().Match == true)
+        {
+            if (!doorIsOpen)
+            {
+                OpenDoor();
+            }
+        }
+	
+	}
+}
